5d5d23569abb3a93ae6fc688904bbf977c73c26d
brahim-slimani <slimani.ibr@gmail.com>
2023-02-04 19:31:05 +0100

    Call chatGPTrequest from index.js entry

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+
+const client = require("./dist/client")
 console.log("Hi from common entry!");
+
+
+client.chatGPTrequest({
+    token: "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",
+    model: "text-davinci-003",
+    prompt: "What time is it ?"
+}).then((response => {
+    console.log("Response =>", response)
+}, error => {
+    console.error("Error =>", error);
+}));
