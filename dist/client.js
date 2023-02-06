60047251543cbfea2740dad68e0bbe50873d4a02
brahim-slimani <slimani.ibr@gmail.com>
2023-02-06 11:52:57 +0100

    Add get models service

--- dist/client.js
+++ dist/client.js
@@ -11,34 +11,46 @@ InvalidApiKeyException.prototype.name = "InvalidApiKeyException";
  * Open AI call interceptor
  */
 const customInterceptor = axios.create();
-customInterceptor.interceptors.request.use(
-    async (config) => {
-        config.baseURL = "https://api.openai.com/v1/completions";
-        config.timeout = 30 * 1000; //in seconds
-        return config;
-    },
-    error => Promise.reject(error)
-);
 
 class ChatGPTApi {
 
     apiKey;
-    constructor({ apiKey }) {
+    organization;
+    constructor({ apiKey, organization }) {
+        this.organization = organization;
         if(apiKey) {
             this.apiKey = apiKey;
         } else {
             throw new InvalidApiKeyException(`Missing OpenAI Api key`)
         }
+
+        customInterceptor.interceptors.request.use(
+            async (config) => {
+                config.baseURL = "https://api.openai.com/v1";
+                config.timeout = 30 * 1000; //in seconds,
+                config.headers = { 'Authorization': `Bearer ${this.apiKey}`, ...(this.organization) && {'OpenAI-Organization': `${this.organization}`} }
+                return config;
+            },
+            error => Promise.reject(error)
+        );
     }
 
     chatGPTrequest = ({ model, prompt }) => {
         return customInterceptor.request({
+            url: "/completions",
             method: "POST",
             headers: { 'Authorization': `Bearer ${this.apiKey}` },
             data: {
                 model: model,
                 prompt: prompt
             }
+        });
+    }
+
+    getModels = () => {
+        return customInterceptor.request({
+            url: "/models",
+            method: "GET",
         });
     }
 }
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
+require('dotenv').config();
 const { ChatGPTApi } = require("./dist/client");
 
 console.log("Hi from common entry!");
 
-const api = new ChatGPTApi({}); //new ChatGPTApi({ apiKey: "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" });
-api.chatGPTrequest({ model: "text-davinci-003", prompt: "What time is it ?" });
+//const api = new ChatGPTApi({ apiKey: "" });
 
-// client.chatGPTrequest({
-//     token: "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",
-//     model: "text-davinci-003",
-//     prompt: "What time is it ?"
-// }).then((response => {
-//     console.log("Response =>", response)
-// }, error => {
-//     console.error("Error =>", error);
-// }));
+async function run () {
+    //let response = await api.chatGPTrequest({ model: "text-davinci-003", prompt: "What time is it ?" });
+    let response = await api.getModels();
+    console.log(".....", response.data);
+}
+
+//run();
+
+console.log("===>", process.env.API_KEY)
