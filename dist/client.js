40f05e596377d7a4258799734fab59ad86b79b48
brahim-slimani <slimani.ibr@gmail.com>
2023-02-08 16:52:39 +0100

    Add JS docs

--- dist/client.js
+++ dist/client.js
@@ -8,15 +8,16 @@ function InvalidApiKeyException(message) {
 InvalidApiKeyException.prototype = new Error();
 InvalidApiKeyException.prototype.name = "InvalidApiKeyException";
 
-/**
- * Open AI call interceptor
- */
-const customInterceptor = axios.create();
-
 class ChatGPTApi {
 
-    apiKey;
-    organization;
+    apiKey; organization;
+
+    /**
+     * ChatGPTApi constructor
+     * @param {Object}, 
+     * apikey: OpenAI API key for authentification. Visit the page https://platform.openai.com/account/api-keys to retrieve the API Key you'll use in your requests,
+     * organization: optional field. For users who belong to multiple organizations, you can pass a this property to specify which organization is used for an API request.
+     */
     constructor({ apiKey, organization }) {
         this.organization = organization;
         if (apiKey) {
@@ -38,20 +39,35 @@ class ChatGPTApi {
 
     service = new ReqService(customInterceptor);
 
-    sendMessage = ({ model, prompt, max_tokens, temperature, nCompeletions }) => new Promise((resolve, reject) => {
+    /**
+     * Given a prompt, the model will return one or more predicted respones, 
+     * and can also return the probabilities of alternative tokens at each position.
+     * @param {Object}  
+     * model: ID of the model to use. You can use the List models API (api.getModels) to see all of your available models, Default is `text-davinci-003`
+     * prompt: The prompt(s) to generate completions for,
+     * max_tokens: The maximum number of tokens to generate in the completion,
+     * temperature: What sampling temperature to use, between 0 and 2, Default is 1,
+     * nCompeletions: How many completions to generate for each prompt. Default is 1
+     * @returns Promise object
+     */
+    sendMessage = ({ model = 'text-davinci-003', prompt, max_tokens, temperature, nCompeletions }) => {
         return this.service.call({
             url: "/completions",
             method: "POST",
             data: {
-                model: model ? model : "text-davinci-003",
+                model: model,
                 prompt: prompt,
                 ...(max_tokens) && { 'max_tokens': max_tokens },
                 ...(temperature) && { 'temperature': temperature },
                 ...(nCompeletions) && { 'n': nCompeletions }
             }
         });
-    });
+    };
 
+    /**
+     * This allows to retrieve a lists the currently available OpenAI models, and provides basic information about each one such as the owner and availability.
+     * @returns Promise object
+     */
     getModels = () => {
         return this.service.call({
             url: "/models",
