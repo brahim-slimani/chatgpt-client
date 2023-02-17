4710cace2d64d048306459762d7ecf490aca0698
brahim-slimani <slimani.ibr@gmail.com>
2023-02-17 15:13:57 +0100

    Clean up index.js

--- index.js
+++ index.js
@@ -1,27 +1,2 @@
 
 module.exports = require("./dist/client");
-
-//require('dotenv').config();
-//const { ChatGPTApi } = require("./dist/client");
-
-//const api = new ChatGPTApi({ apiKey: process.env.API_KEY });
-
-// async function run () {
-//     let response = await api.chatGPTrequest({ model: "text-davinci-003", prompt: "What time is it ?" });
-//     api.getModels(resp => {
-//         console.log("===>", resp)
-
-//     }, error => {
-//         console.log("...>", error)
-//     });
-
-//run();
-
-// api.getModels().then(resp => {
-//     console.log("===>", resp)
-// }, error => {
-//     console.log("...>", error)
-// });
-
-//https://raw.githubusercontent.com/devicons/devicon/master/icons/gitlab/gitlab-original.svg
-//https://github-readme-stats.vercel.app/api/pin/?username=brahim-slimani&repo=jwt-decoder
